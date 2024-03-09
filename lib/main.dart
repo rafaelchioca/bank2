bc72269239f2e690b4ceba838cda94ed286679aa
Rafael Chioca <rafaelmchioca@gmail.com>
2024-03-08 21:14:04 -0300

    :sparkles: feat: Teste de estilização

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,48 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          body: const Column(
+            children: [
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on, color: Colors.green),
+                  title: Text('1015-8'),
+                  subtitle: Text('R\$ 875,00'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on, color: Colors.green),
+                  title: Text('2030-7'),
+                  subtitle: Text('R\$ 555,00'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on, color: Colors.green),
+                  title: Text('7890-1'),
+                  subtitle: Text('R\$ 1.150,00'),
+                ),
+              ),
+            ],
+          ),
           appBar: AppBar(
-            title: Text('Transferências'),
+            backgroundColor: Colors.orange,
+            title: const Text(
+              'Transferências',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
           floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.orange,
             onPressed: () {},
-            child: Icon(Icons.add),
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
           ),
         ),
       ),
