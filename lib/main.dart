e22a999162b5de3d58f72cbdcdfb0ccddd5bca6a
Rafael Chioca <rafaelmchioca@gmail.com>
2024-03-09 11:02:10 -0300

    Refatoração - Aula 09/03/2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,116 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: const Column(
-            children: [
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on, color: Colors.green),
-                  title: Text('1015-8'),
-                  subtitle: Text('R\$ 875,00'),
-                ),
+void main() => runApp(const BankApp());
+
+class BankApp extends StatelessWidget {
+  const BankApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  const FormularioTransferencia({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text(
+          'Nova Transferência',
+          style: TextStyle(
+              color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              style: TextStyle(fontSize: 18),
+              decoration: InputDecoration(
+                labelText: 'Número da conta',
+                hintText: '0000',
+                icon: Icon(Icons.account_balance),
               ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on, color: Colors.green),
-                  title: Text('2030-7'),
-                  subtitle: Text('R\$ 555,00'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on, color: Colors.green),
-                  title: Text('7890-1'),
-                  subtitle: Text('R\$ 1.150,00'),
-                ),
-              ),
-            ],
-          ),
-          appBar: AppBar(
-            backgroundColor: Colors.orange,
-            title: const Text(
-              'Transferências',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold),
+              keyboardType: TextInputType.number,
             ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.orange,
-            onPressed: () {},
-            child: const Icon(
-              Icons.add,
-              color: Colors.white,
+            TextField(
+              style: TextStyle(fontSize: 18),
+              decoration: InputDecoration(
+                labelText: 'Valor R\$',
+                hintText: '0.00',
+                icon: Icon(Icons.monetization_on),
+              ),
+              keyboardType: TextInputType.number,
             ),
-          ),
+          ],
         ),
       ),
     );
+  }
+}
+
+class ListaTransferencias extends StatelessWidget {
+  const ListaTransferencias({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text(
+          'Transferências',
+          style: TextStyle(
+              color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Column(
+        children: [
+          ItemTransferencia(
+            Transferencia(1500.50, 6874),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange,
+        onPressed: () {},
+        child: const Icon(Icons.add, color: Colors.white),
+      ),
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  const ItemTransferencia(this._transferencia, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.monetization_on, color: Colors.green),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
