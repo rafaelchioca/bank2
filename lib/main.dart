98bcacd7a2b3ebfeeff6a7e6f761dedc770e50c1
Rafael Chioca <rafaelmchioca@gmail.com>
2024-03-22 22:14:37 -0300

    (22/03) Refatoração do código

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,18 @@ class BankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: FormularioTransferencia(),
+        body: ListaTransferencias(),
       ),
     );
   }
 }
 
 class FormularioTransferencia extends StatelessWidget {
-  const FormularioTransferencia({super.key});
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  FormularioTransferencia({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -27,33 +31,71 @@ class FormularioTransferencia extends StatelessWidget {
         title: const Text(
           'Nova Transferência',
           style: TextStyle(
-              color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+              color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
         ),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              style: TextStyle(fontSize: 18),
-              decoration: InputDecoration(
-                labelText: 'Número da conta',
-                hintText: '0000',
-                icon: Icon(Icons.account_balance),
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-              style: TextStyle(fontSize: 18),
-              decoration: InputDecoration(
-                labelText: 'Valor R\$',
-                hintText: '0.00',
-                icon: Icon(Icons.monetization_on),
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ],
+      body: Column(
+        children: [
+          Editor(
+              controlador: _controladorCampoNumeroConta,
+              rotulo: 'Número da Conta',
+              dica: '0000',
+              icone: Icons.account_balance),
+          Editor(
+              controlador: _controladorCampoValor,
+              rotulo: 'Valor R\$',
+              dica: '0.00',
+              icone: Icons.monetization_on),
+          ElevatedButton(
+            child: const Text('Confirmar'),
+            onPressed: () {
+              _criaTransferencia(
+                  _controladorCampoNumeroConta, _controladorCampoValor);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _criaTransferencia(conrtroladorCampoNumeroConta, controladorCampoValor) {
+    final int? numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+    final double? valor = double.tryParse(controladorCampoValor.text);
+    if (numeroConta != null && valor != null) {
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+    }
+  }
+}
+
+class Editor extends StatelessWidget {
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData? icone;
+
+  const Editor(
+      {required this.controlador,
+      required this.rotulo,
+      required this.dica,
+      this.icone,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controlador,
+        style: const TextStyle(
+          fontSize: 20.0,
         ),
+        decoration: InputDecoration(
+          icon: icone != null ? Icon(icone, color: Colors.green) : null,
+          labelText: rotulo,
+          hintStyle: const TextStyle(color: Colors.grey),
+          hintText: dica,
+        ),
+        keyboardType: TextInputType.number,
       ),
     );
   }
@@ -71,19 +113,26 @@ class ListaTransferencias extends StatelessWidget {
         title: const Text(
           'Transferências',
           style: TextStyle(
-              color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+              color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
         ),
       ),
       body: Column(
         children: [
           ItemTransferencia(
-            Transferencia(1500.50, 6874),
+            Transferencia(100.0, 1000),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.orange,
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) {
+              return FormularioTransferencia();
+            }),
+          );
+        },
         child: const Icon(Icons.add, color: Colors.white),
       ),
     );
@@ -113,4 +162,9 @@ class Transferencia {
   final int numeroConta;
 
   Transferencia(this.valor, this.numeroConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
 }
